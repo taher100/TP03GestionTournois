da69d9b6b13645e56d486e4219a6eef39c74f0b8
charlenelecouturier <33778377+charlenelecouturier@users.noreply.github.com>
2019-12-03 22:20:32 -0500

    update view index

--- TP03GestionTournois/Controllers/HomeController.cs
+++ TP03GestionTournois/Controllers/HomeController.cs
@@ -23,10 +23,7 @@ namespace TP03GestionTournois.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
